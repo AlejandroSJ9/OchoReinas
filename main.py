0cb616bf2e4c98f17c5f201cd932d74c58b29f6e
Alejandro Salas Jurado <alejandro.salas090603@gmail.com>
2023-05-29 23:10:33 -0500

    proyecto al 75%

---
+++ main.py
@@ -0,0 +1,45 @@
+def es_seguro(tablero, fila, columna):
+    for i in range(fila):
+        if tablero[i] == columna:
+            return False
+    
+    for i in range(fila):
+        if abs(tablero[i] - columna) == abs(i - fila):
+            return False
+    
+    return True
+
+
+def colocar_reina(tablero, fila):
+    
+    if fila >= 8:
+        return True
+    
+    for columna in range(8):
+        if es_seguro(tablero, fila, columna):
+            tablero[fila] = columna
+            if colocar_reina(tablero, fila + 1):
+                return True
+            tablero[fila] = -1
+    
+    return False
+
+
+def resolver_ocho_reinas():
+    
+    tablero = [-1] * 8
+    
+    if colocar_reina(tablero, 0):
+        print("Solución encontrada:")
+        for fila in range(8):
+            for columna in range(8):
+                if tablero[fila] == columna:
+                    print("2", end=" ")
+                else:
+                    print(".", end=" ")
+            print()
+    else:
+        print("No se encontró solución.")
+
+
+resolver_ocho_reinas()
